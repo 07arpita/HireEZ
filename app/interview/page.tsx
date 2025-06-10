d6b3268763ea524577cf21df48f4ff0d6c88c8e6
Deepanshu Sharma <sharmadeepanshu9901@gmail.com>
2025-06-10 19:14:11 +0530

    Fix interview page module error

--- app/interview/page.tsx
+++ app/interview/page.tsx
@@ -1,1 +1,12 @@
- 
+import React from 'react';
+
+const InterviewPage = () => {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center p-24">
+      <h1>Interview Page</h1>
+      <p>This page is under construction.</p>
+    </div>
+  );
+};
+
+export default InterviewPage; 
